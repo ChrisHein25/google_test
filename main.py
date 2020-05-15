5936c0f0b4e5685128c9634cc45fe5ae2fff59c5
ChrisHein25 <chris.hein@comcast.net>
2020-05-14 22:13:04 -0500

    changes

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask, flash, render_template, request, session
+from flask import Flask, g, flash, render_template, request, session
 from flask_bootstrap import Bootstrap
 import yaml
 import os
+import sqlite3
+#import dbfunc
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -9,8 +11,43 @@ Bootstrap(app)
 # generate random secret key so flash can run
 app.config['SECRET_KEY'] = os.urandom(24)
 
-# SQL Database
+# SQL Database Functions
+DATABASE = 'database.db'
 
+def addWord(word):
+    con = sqlite3.connect(DATABASE)
+    cur = con.cursor()
+    cur.execute("INSERT INTO words(word) VALUES(?)", (word,))
+    con.commit()
+    con.close()
+
+def deleteWord(word):
+    # NEED TO ADD TRY EXCEPT BLOCK W BOOL LOGIC
+    con = sqlite3.connect(DATABASE)
+    cur = con.cursor()
+    cur.execute("DELETE FROM words WHERE word=?", (word,))
+    con.commit()
+    con.close()
+
+def printWords(): # prints entire table to command line
+    con = sqlite3.connect(DATABASE)
+    cur = con.cursor()
+    cur.execute("SELECT * FROM words")
+    words = cur.fetchall()
+    con.close()
+    print(words)
+    return
+
+def getWordTable():
+    con = sqlite3.connect(DATABASE)
+    cur = con.cursor()
+    cur.execute("SELECT * FROM words")
+    wordTable = cur.fetchall()
+    con.close()
+    return wordTable
+
+
+##### Browser Routing #####
 
 @app.route('/')
 def index():
@@ -23,20 +60,21 @@ def words():
         wordInfo = request.form
         inputWord = wordInfo['inputWord']
         buttonValue = wordInfo['submit-button']
-        print(inputWord, buttonValue)
+        #print(inputWord, buttonValue)
         if buttonValue == 'add':
-            flash('Word Added.', 'success')
             # add word to database
+            addWord(str(inputWord))
+            flash('Word Added.', 'success')
         elif buttonValue == 'delete':
-            flash('Word Deleted.', 'success')
             # delete word from database
+            deleteWord(str(inputWord))
+            flash('Word Deleted.', 'success')
         else:
             flash('Something went wrong. Your word was not added.', 'danger')
-
-        #create variable that holds database entries and pass to html page
-        return render_template('words.html')
-    #create variable that holds database entries and pass to html page
-    return render_template('words.html', testVar = testVar, employeeList = employeeList)
+        wordTable = getWordTable() # table in list
+        return render_template('words.html', wordTable = wordTable)
+    wordTable = getWordTable() # table in list
+    return render_template('words.html', wordTable = wordTable)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ schema.sql
@@ -0,0 +1,5 @@
+drop table if exists words;
+    create table words (
+    id integer primary key autoincrement,
+    word text not null
+);
